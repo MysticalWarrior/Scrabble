94830237db560d7614976954b96ac2f0aaa6a062
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2020-05-05 18:44:52 -0400

    Inital Code Commit
    this initial code contains a *working* program; 
    the program is able to read the data, it is able to take a user input, and sort through the data, filtering out any words that don't contain the first letter.
    I guess then really, it doesnt work...
    
    *Need to stop reading and storing all the words with more than 7 letters
    *And deal with other letters (because it only does the first) - filter out any words that aren't entirely made up of tiles...

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scrabble
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public char[] playerHand;
+        public string[] validWords;
+        public MainWindow()
+        {
+            InitializeComponent();
+            playerHand = new char[7];
+            validWords = pullDataFromWeb();
+            MessageBox.Show(validWords.Length.ToString());
+
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            int validWordCounter = 0;
+            try
+            {
+                //copies the first seven letters of the input textbox to the player hand.
+                playerHand = txtInput.Text.Substring(0, 7).ToCharArray();
+
+                for (int i = 0; i < validWords.Length; i++)
+                {
+                    if (validWords[i].Contains(playerHand[0].ToString()))
+                    {
+                        lblOutput.Content += validWords[i] + '\r';
+                        validWordCounter++;
+                    }
+                }
+                lblOutput.Content = validWordCounter.ToString() + " playable words\r" + lblOutput.Content;
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("unable to obtain tiles from input");
+            }
+        }
+
+        private string[] pullDataFromWeb()
+        {
+            List<string> temp = new List<string>();
+            try
+            {
+                System.Net.WebClient wc = new System.Net.WebClient();
+                System.IO.StreamReader sr = new System.IO.StreamReader(wc.OpenRead("http://darcy.rsgc.on.ca/ACES/ICS4U/SourceCode/Words.txt"));
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    temp.Add(line);
+                    //MessageBox.Show(line + " " + temp[(temp.Count() - 1)]);
+                }
+                sr.Close();
+            }
+            catch (Exception) { MessageBox.Show("Error reading words from web-based file"); throw; }
+
+            return temp.ToArray();
+        }
+    }
+}
